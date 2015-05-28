39051445fa3c7f31c567b3df806873bba68ec417
Maciej Wawryk <maddox321@gmail.com>
2015-05-28 22:10:56 +0200

    first commit

---
+++ java/com/maddox/navigon/MapAppActivity.java
@@ -0,0 +1,208 @@
+package com.maddox.navigon;
+
+/**
+ * Code owner Maciej Wawryk
+ * Contact: wawryk2@gmail.com
+ * Contact: 505525431
+ * Created by Maddox on 2015-05-04.
+ * In project TMC.
+ */
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Display;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ZoomControls;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.maddox.navigon.MapView;
+import com.maddox.navigon.MapViewLocationListener;
+import com.maddox.navigon.PointD;
+import com.maddox.navigon.TilesProvider;
+
+public class MapAppActivity extends Activity{
+
+    private GPSTracker gpsTracker;
+
+    private final class Save{
+        public final static String GPS_LON = "gpsLon";
+        public final static String GPS_LAT = "gpsLAT";
+    }
+
+    //do zachowania właściwości aby przy akcji on restore
+    private final class Pref{
+        public final static String SEEK_LON = "seek_lon";
+        public final static String SEEK_LAT = "seek_lat";
+        public final static String ZOOM = "zoom";
+    }
+
+    MapView mapView;
+    TilesProvider tilesProvider;
+
+    MapViewLocationListener locationListener;
+
+    Location savedGpsLocation;
+
+    ZoomControls zoomControls;
+
+    //zoom
+    View.OnClickListener zoomIn_Click = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            mapView.zoomIn();
+        }
+    };
+
+    View.OnClickListener zoomOut_Click = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            mapView.zoomOut();
+        }
+    };
+
+    @Override
+    protected void onResume(){
+        setContentView(R.layout.main);
+
+        mapView = (MapView) findViewById(R.id.map);
+        zoomControls = (ZoomControls) findViewById(R.id.zoom);
+
+        initViews();
+        restoreMapViewSettings();
+        super.onResume();
+    }
+
+    void initViews(){
+        //Bitmap marker = BitmapFactory.decodeResource(getResources(), R.drawable.marker);
+//        String path = Environment.getExternalStorageDirectory() + "/mapapp/Lol.sqlitedb";
+        String path = "/storage/sdcard1/mapapp/Lol.sqlitedb";
+        tilesProvider = new TilesProvider(path);
+
+        if (savedGpsLocation != null) mapView.setGpsLocation(gpsTracker.getLongitude(), gpsTracker.getLongitude());
+
+        mapView.setTilesProvider(tilesProvider);
+
+        mapView.refresh();
+        zoomControls.setOnZoomInClickListener(zoomIn_Click);
+        zoomControls.setOnZoomOutClickListener(zoomOut_Click);
+    }
+
+    @Override
+    protected void onPause(){
+        // Save settings before leaving
+        saveMapViewSettings();
+
+        // Mainly releases the MapView pointer inside the listener
+        locationListener.stop();
+
+        // Unregistering our listener
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.removeUpdates(locationListener);
+
+        // Closes the source of the tiles (Database in our case)
+        tilesProvider.close();
+        // Clears the tiles held in the tilesProvider
+        tilesProvider.clear();
+
+        // Release mapView pointer
+        mapView = null;
+
+        super.onPause();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        // Zoom
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_Z){
+            mapView.zoomIn();
+            return true;
+        }
+        // Zoom
+        else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_X){
+            mapView.zoomOut();
+            return true;
+        }
+        // Enable auto follow
+        if (keyCode == KeyEvent.KEYCODE_H || keyCode == KeyEvent.KEYCODE_FOCUS){
+            mapView.followMarker();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    // Called manually to restore settings from SharedPreferences
+    void restoreMapViewSettings(){
+        SharedPreferences pref = getSharedPreferences("View_Settings", MODE_PRIVATE);
+
+        double lon, lat;
+        int zoom;
+
+        gpsTracker = new GPSTracker(getBaseContext());
+        mapView.setGpsLocation(gpsTracker.getLongitude(), gpsTracker.getLatitude());
+        mapView.invalidate();
+        mapView.followMarker();
+
+//        lon = Double.parseDouble(pref.getString(Pref.SEEK_LON, "0"));
+//        lat = Double.parseDouble(pref.getString(Pref.SEEK_LAT, "0"));
+//        zoom = pref.getInt(Pref.ZOOM, 0);
+//
+//        mapView.setSeekLocation(lon, lat);
+//        mapView.setZoom(zoom);
+        mapView.refresh();
+    }
+
+    // Called manually to save settings in SharedPreferences
+    void saveMapViewSettings(){
+        SharedPreferences.Editor editor = getSharedPreferences("View_Settings", MODE_PRIVATE).edit();
+
+        PointD seekLocation = mapView.getSeekLocation();
+        editor.putString(Pref.SEEK_LON, Double.toString(seekLocation.x));
+        editor.putString(Pref.SEEK_LAT, Double.toString(seekLocation.y));
+        editor.putInt(Pref.ZOOM, mapView.getZoom());
+
+        editor.commit();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        if (mapView.getGpsLocation() != null){
+            outState.putDouble(Save.GPS_LON, mapView.getGpsLocation().getLongitude());
+            outState.putDouble(Save.GPS_LAT, mapView.getGpsLocation().getLatitude());
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        double gpsLon, gpsLat, gpsAlt;
+        float gpsAcc;
+
+        gpsLon = savedInstanceState.getDouble(Save.GPS_LON, 999);
+        gpsLat = savedInstanceState.getDouble(Save.GPS_LAT, 999);
+
+        if (gpsLon != 999 && gpsLat != 999){
+            savedGpsLocation.setLongitude(gpsTracker.getLongitude());
+            savedGpsLocation.setLatitude(gpsTracker.getLatitude());
+        }
+
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+}
---
+++ java/com/maddox/navigon/MapView.java
@@ -0,0 +1,488 @@
+/*
+ * MapApp : Simple offline map application, made by Hisham Ghosheh for tutorial purposes only
+ * Tutorial on my blog
+ * http://ghoshehsoft.wordpress.com/2012/03/09/building-a-map-app-for-android/
+ * 
+ * Class tutorial:
+ * http://ghoshehsoft.wordpress.com/2012/04/06/mapapp5-mapview-and-activity/
+ */
+
+package com.maddox.navigon;
+
+import java.util.Collection;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MapView extends View
+{
+    // Needed to pass to View constructor
+    protected Context context;
+
+    // MapView dimensions
+    protected int viewWidth, viewHeight;
+
+    // Provides us with tiles
+    protected TilesProvider tileProvider;
+
+    // Handles calculations
+    protected TilesManager tileManager;
+
+    // Different paints
+    protected Paint fontPaint;
+    protected Paint bitmapPaint = new Paint();
+    protected Paint circlePaint = new Paint();
+
+    // The location of the view center in longitude, latitude
+    protected PointD seekLocation = new PointD(18.612363, 54.371676);
+    // Location of the phone using Gps data
+    protected Location gpsLocation = null;
+    // If true then seekLocation will always match gpsLocation
+    protected boolean autoFollow = false;
+
+    // An image to draw at the phone's position
+    protected Bitmap positionMarker;
+
+    // touch position values kept for panning\dragging
+    protected PointD lastTouchPos = new PointD(-1, -1);
+
+    public MapView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+
+        // We don't know view dimensions yet, super.onMeasure will be called
+        viewWidth = viewHeight = -1;
+
+        if (!isInEditMode())
+        {
+            int zoomLevel = 0;
+            TypedArray arr = context.getTheme().obtainStyledAttributes(attrs, R.styleable.MapView, 0,0);
+            try
+            {
+                zoomLevel = arr.getInt(R.styleable.MapView_zoomLevel, 0);
+                Drawable d = arr.getDrawable(R.styleable.MapView_marker);
+                if (d != null) positionMarker = ((BitmapDrawable) d).getBitmap();
+            }
+            finally
+            {
+                arr.recycle();
+            }
+
+            tileManager = new TilesManager(256, viewWidth, viewHeight);
+            tileManager.setZoom(zoomLevel);
+        }
+
+        initPaints();
+    }
+
+    public MapView(Context context, int viewWidth, int viewHeight, TilesProvider tilesProvider, Bitmap positionMarker)
+    {
+        super(context);
+        this.context = context;
+
+        // Tiles provider is passed not created.
+        // The idea is to hide the actual tiles source from the view
+        // This way the view doesn't care whether the source is a database or
+        // the internet
+        this.tileProvider = tilesProvider;
+
+        // These values will be used later
+        this.viewWidth = viewWidth;
+        this.viewHeight = viewHeight;
+
+        // Get the marker image
+        this.positionMarker = positionMarker;
+
+        // Creating a TilesManager assuming that the tile size is 256*256.
+        // You might want to pass tile size as a parameter or even calculate it
+        // somehow
+        tileManager = new TilesManager(256, viewWidth, viewHeight);
+
+        // Initializes paints
+        initPaints();
+
+        // Fetching tiles from the tilesProvider
+        fetchTiles();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        // Case1 : width and height are provided in the constructor
+        if (viewWidth != -1 && viewHeight != -1)
+        {
+            // Setting width,height that was passed in the constructor as the
+            // view's dimensions
+            setMeasuredDimension(viewWidth, viewHeight);
+        }
+        // Case2: view was created using XML
+        else
+        {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    void initPaints()
+    {
+        // Font paint is used to draw text
+        fontPaint = new Paint();
+        fontPaint.setColor(Color.DKGRAY);
+        fontPaint.setShadowLayer(1, 1, 1, Color.BLACK);
+        fontPaint.setTextSize(20);
+
+        // Used to draw a semi-transparent circle at the phone's gps location
+        circlePaint.setARGB(70, 170, 170, 80);
+        circlePaint.setAntiAlias(true);
+    }
+
+    void fetchTiles()
+    {
+        // Update tilesManager to have the center of the view as its location
+        tileManager.setLocation(seekLocation.x, seekLocation.y);
+
+        // Get the visible tiles indices as a Rect
+        Rect visibleRegion = tileManager.getVisibleRegion();
+
+        if (tileProvider == null) return;
+
+        // Tell tiles provider what tiles we need and which zoom level.
+        // The tiles will be stored inside the tilesProvider.
+        // We can get those tiles later when drawing the view
+        tileProvider.fetchTiles(visibleRegion, tileManager.getZoom());
+    }
+
+    public void setTilesProvider(TilesProvider tilesProvider)
+    {
+        this.tileProvider = tilesProvider;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        if (this.isInEditMode())
+        {
+            canvas.drawARGB(255, 255, 255, 255);
+
+            String str = "MapView: Design Mode";
+
+            // Text centering, Modified version of :
+            // http://stackoverflow.com/a/11121873
+            int xPos = (int) ((canvas.getWidth() / 2f) - fontPaint.measureText(str) / 2f);
+            int yPos = (int) ((canvas.getHeight() / 2) - ((fontPaint.descent() + fontPaint.ascent()) / 2));
+
+            canvas.drawText(str, xPos, yPos, fontPaint);
+            return;
+        }
+
+        if (tileProvider == null)
+        {
+            canvas.drawARGB(255, 250, 111, 103);
+
+            String str = "TilesProvider not set";
+
+            // Text centering, Modified version of :
+            // http://stackoverflow.com/a/11121873
+            int xPos = (int) ((canvas.getWidth() / 2f) - fontPaint.measureText(str) / 2f);
+            int yPos = (int) ((canvas.getHeight() / 2) - ((fontPaint.descent() + fontPaint.ascent()) / 2));
+
+            canvas.drawText(str, xPos, yPos, fontPaint);
+
+            return;
+        }
+
+        // Clear the view to grey
+        canvas.drawARGB(255, 100, 100, 100);
+
+		/*
+		 * To draw the map we need to find the position of the pixel representing the center of the view.
+		 * We need the position to be relative to the full world map, lets call this pixel position "pix"
+		 * pix.x will range from 0 to (2^zoom)*tileSize-1, same for pix.y
+		 * To draw anything on the map we subtract pix from the original position
+		 * It's just like dragging the map so that the pixel representing the gps location gets into the center of the view
+		*/
+
+        // In a square world map,
+        // we need to know pix location as two values from 0.0 to 1.0
+        PointD pixRatio = TilesManager.calcRatio(seekLocation.x, seekLocation.y);
+
+        // Full world map width in pixels
+        int mapWidth = tileManager.mapSize() * 256;
+        Point pix = new Point((int) (pixRatio.x * mapWidth), (int) (pixRatio.y * mapWidth));
+
+		/*
+		 * Subtracting pix from each tile position will result in pix being drawn at the top left corner of the view
+		 * To drag it to the center we add (viewWidth/2, viewHeight/2) to the final result
+		 * pos.x = pos.x - pix.x + viewWidth/2f
+		 * pos.x = pox.x - (pix.x - viewWidth/2f)
+		 * ---> offset.x =  (pix.x - viewWidth/2f)
+		 * same for offset.y
+		 */
+
+        Point offset = new Point((int) (pix.x - viewWidth / 2f), (int) (pix.y - viewHeight / 2f));
+        // offset is now ready to use
+
+        // Drawing tiles in a separate function to make the code more readable
+        drawTiles(canvas, offset);
+
+        // Draw the marker that pinpoints the user's location
+        drawMarker(canvas, offset);
+    }
+
+    void drawTiles(Canvas canvas, Point offset)
+    {
+        // Get tiles from the Hashtable inside tilesProvider
+        Collection<Tile> tilesList = tileProvider.getTiles().values();
+
+        // x,y are the calculated offset
+
+        // Go trough all the available tiles
+        for (Tile tile : tilesList)
+        {
+            // We act as if we're drawing a map of the whole world at a specific
+            // zoom level
+            // The top left corner of the map occupies the pixel (0,0) of the
+            // view
+            int tileSize = tileManager.getTileSize();
+            long tileX = tile.x * tileSize;
+            long tileY = tile.y * tileSize;
+
+            // Subtract offset from the previous calculations
+            long finalX = tileX - offset.x;
+            long finalY = tileY - offset.y;
+
+            // Draw the bitmap of the tiles using a simple paint
+            canvas.drawBitmap(tile.img, finalX, finalY, bitmapPaint);
+        }
+    }
+
+    void drawMarker(Canvas canvas, Point offset)
+    {
+        // x,y are the calculated offset
+
+        // Proceed only if a gps fix is available
+        if (gpsLocation != null)
+        {
+            // Get marker position in pixels as if we're going to draw it on a
+            // world map where the top left corner of the map occupies the (0,0)
+            // pixel of the view
+            Point markerPos = tileManager.lonLatToPixelXY(gpsLocation.getLongitude(), gpsLocation.getLatitude());
+
+            // Add offset to the marker position
+            int markerX = markerPos.x - offset.x;
+            int markerY = markerPos.y - offset.y;
+
+            // If marker bitmap exists
+            if (positionMarker != null)
+            {
+                // Draw the marker and make sure you draw the center of the
+                // marker
+                // at the marker location
+                canvas.drawBitmap(positionMarker, markerX - positionMarker.getWidth() / 2, markerY - positionMarker.getHeight() / 2,
+                        bitmapPaint);
+            }
+            else
+            {
+                // Draw an ugly circle if no bitmap is set :P
+                canvas.drawCircle(markerX, markerY, 10, bitmapPaint);
+            }
+
+            // Draw the marker and make sure you draw the center of the marker
+            // at the marker location
+
+            // Around the marker we will draw a circle representing the accuracy of the gps fix
+            // We first calculate its radius
+
+            // Calculate how many meters one pixel represents
+            float ground = (float) tileManager.calcGroundResolution(gpsLocation.getLatitude());
+
+            // Location.getAccuracy() returns the accuracy in meters.
+            float rad = gpsLocation.getAccuracy() / ground;
+
+            canvas.drawCircle(markerX, markerY, rad, circlePaint);
+
+            // Just drawing location info
+            int pen = 1;
+            canvas.drawText("lon:" + gpsLocation.getLongitude(), 0, 20 * pen++, fontPaint);
+            canvas.drawText("lat:" + gpsLocation.getLatitude(), 0, 20 * pen++, fontPaint);
+//            canvas.drawText("alt:" + gpsLocation.getAltitude(), 0, 20 * pen++, fontPaint);
+            canvas.drawText("Zoom:" + tileManager.getZoom(), 0, 20 * pen++, fontPaint);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        int action = event.getAction();
+
+        if (action == MotionEvent.ACTION_DOWN)
+        {
+            // Keep touch position for later use (dragging)
+            lastTouchPos.x = (int) event.getX();
+            lastTouchPos.y = (int) event.getY();
+            Log.d("asd","asd");
+            tileManager. lonLatToPixelXY((int) event.getX(), (int) event.getY());
+
+
+            return true;
+        }
+        else if (action == MotionEvent.ACTION_MOVE)
+        {
+            autoFollow = false;
+            PointD current = new PointD(event.getX(), event.getY());
+
+            // Find how many pixels the users finger moved in both x and y
+            PointD diff = new PointD(current.x - lastTouchPos.x, current.y - lastTouchPos.y);
+
+            // In a full wolrd map, get the position of the center of the view in pixels
+            Point pixels1 = tileManager.lonLatToPixelXY(seekLocation.x, seekLocation.y);
+
+            // Subtract diff from that position
+            Point pixels2 = new Point(pixels1.x - (int) diff.x, pixels1.y - (int) diff.y);
+
+            // Recnovert the final result to longitude, latitude point
+            PointD newSeek = tileManager.pixelXYToLonLat((int) pixels2.x, (int) pixels2.y);
+
+            // Finally move the center of the view to the new location
+            seekLocation = newSeek;
+
+            // Refresh the view
+            fetchTiles();
+            invalidate(); // Causes the view to redraw itself
+
+            // Prepare for the next drag event
+            lastTouchPos.x = current.x;
+            lastTouchPos.y = current.y;
+
+            return true;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    // Fetch the tiles then draw, don't call to often
+    public void refresh()
+    {
+        fetchTiles();
+        invalidate();
+    }
+
+    // Like refresh but called from a non UI thread
+    public void postRefresh()
+    {
+        fetchTiles();
+        postInvalidate();
+    }
+
+    // Simply sets seek location to gpsLocation (if exists)
+    public void followMarker()
+    {
+        if (gpsLocation != null)
+        {
+            seekLocation.x = gpsLocation.getLongitude();
+            seekLocation.y = gpsLocation.getLatitude();
+            autoFollow = true;
+
+            fetchTiles();
+            invalidate();
+        }
+    }
+
+    public void zoomIn()
+    {
+        tileManager.zoomIn();
+        onMapZoomChanged();
+    }
+
+    public void zoomOut()
+    {
+        tileManager.zoomOut();
+        onMapZoomChanged();
+    }
+
+    protected void onMapZoomChanged()
+    {
+        if (tileProvider != null) tileProvider.clear();
+
+        fetchTiles();
+        invalidate();
+    }
+
+    // Returns the gps coordinates of the user
+    public Location getGpsLocation()
+    {
+        return gpsLocation;
+    }
+
+    // Returns the gps coordinates of our view center
+    public PointD getSeekLocation()
+    {
+        return seekLocation;
+    }
+
+    // Centers the given gps coordinates in our view
+    public void setSeekLocation(double longitude, double latitude)
+    {
+        seekLocation.x = longitude;
+        seekLocation.y = latitude;
+    }
+
+    // Sets the marker position
+//    public void setGpsLocation(Location location)
+//    {
+//        setGpsLocation(location.getLongitude(), location.getLatitude(), location.getAltitude(), location.getAccuracy());
+//    }
+
+    // Sets the marker position
+    public void setGpsLocation(double longitude, double latitude)//, double altitude, float accuracy)
+    {
+        if (gpsLocation == null) gpsLocation = new Location("");
+        gpsLocation.setLongitude(longitude);
+        gpsLocation.setLatitude(latitude);
+//        gpsLocation.setAltitude(altitude);
+//        gpsLocation.setAccuracy(accuracy);
+
+        if (autoFollow) followMarker();
+
+    }
+
+    public int getZoom()
+    {
+        return tileManager.getZoom();
+    }
+
+    public void setZoom(int zoom)
+    {
+        tileManager.setZoom(zoom);
+        onMapZoomChanged();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom)
+    {
+        super.onLayout(changed, left, top, right, bottom);
+
+        if (!this.isInEditMode())
+        {
+            // Update the tiles manager
+            this.viewWidth = getWidth();
+            this.viewHeight = getHeight();
+
+            tileManager.setDimensions(viewWidth, viewHeight);
+
+            refresh();
+        }
+    }
+}
